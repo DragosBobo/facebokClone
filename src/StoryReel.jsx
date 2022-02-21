dc826774a2da53af3c40600c073c308fbceb2d24
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-21 07:51:22 -0800

    still working here :)

--- src/StoryReel.jsx
+++ src/StoryReel.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "./StoryReel.css"
+import Story from './Story'
 function StoryReel() {
   return (
-    <div>StoryReel</div>
+    <div className='storyReel'>
+                <Story image="https://images.unsplash.com/photo-1553272725-086100aecf5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+                        profileSrc=" https://scontent.ftsr1-1.fna.fbcdn.net/v/t39.30808-6/272292310_1900352856818937_1646822497069197698_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=K_ozQusEWiIAX_B2FeY&tn=l-80_JORztCArx6y&_nc_ht=scontent.ftsr1-1.fna&oh=00_AT_0CQ3jbiaSETCIvUzb2t4wWJ4NEDUb4P88btgVqx-8Zw&oe=62198E67 "
+                        title="AleCs"
+                />
+                <Story image=" https://images.unsplash.com/photo-1645420420620-a7debd2b0363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                profileSrc="https://scontent.ftsr1-1.fna.fbcdn.net/v/t39.30808-1/s320x320/271151128_3645272562264648_8402742632714446117_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=7206a8&_nc_ohc=yt_rH0TdCRsAX_2tJ-L&_nc_ht=scontent.ftsr1-1.fna&oh=00_AT9JSS49dfBlZ3YDBPU1mKyXkwxCeAk2CV_h7_1I7T4EhQ&oe=6217CBA2 "
+                title="Andrei"
+                />
+                <Story image="https://images.unsplash.com/photo-1645397925375-8bcc8584c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80 "
+                profileSrc="https://scontent.ftsr1-2.fna.fbcdn.net/v/t1.18169-1/c0.0.260.260a/20664432_683129138552830_755641267856127665_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=7206a8&_nc_ohc=tMWuyg_Z_RUAX8oDkF8&_nc_ht=scontent.ftsr1-2.fna&oh=00_AT_4dmAd9rBJ_Z-9i0qeD0RVS3TXU_tamd9MUjzlkcJIWg&oe=623AA886 "
+                title="Remus"
+                />
+    </div>
   )
 }
 
