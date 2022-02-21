26cafdc3d45a5bcaf5d5fbfd2775326949435556
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-21 11:13:00 -0800

    sitll workign here ulti done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets'
+import Login from './Login';
 function App() {
+  const user="dragos";
   return (
     <div className="app">
+      {!user ?
+      <Login/>
+      : (
+        <>
         <Header/>
      <div className='app__body'>
         <Sidebar/>
         <Feed/>
-     </div>
-
+        <Widgets/>
+     </div> </>
+)}
     </div>
   );
 }
---
+++ src/Widgets.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Widgets.css";
+function Widgets() {
+  return (
+    <div className='widgets'>
+       <div className="widgets__container">
+       <img src=" https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+       
+       </div>
+       <h3>Buy new books</h3>
+    </div>
+  )
+}
+
+export default Widgets
