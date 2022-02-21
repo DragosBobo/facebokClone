86c6ecb55920d25a053c140716e6712a72316597
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-21 12:02:13 -0800

    done frontend

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,23 @@
+
 import React from 'react'
+
 import "./Login.css";
+import { Button } from '@mui/material';
+
 function Login() {
+
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+<div className="login__logo">
+
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg" />
+
+    
+    
+</div>
+<Button type='submit' >Sign in</Button>
+
+    </div>
   )
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCjhmJPGIrEXeLl7nO2bg8j-Ze6g3B-YEA",
     authDomain: "facebookclone2-39460.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:176282230198:web:a9bdb2df2912ecb9a5f517",
     measurementId: "G-CENBKSN3BH"
   };
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const db =firebaseApp.firestore();
+
+  const auth =firebase.auth();
+  const provider =new firebase.auth.GoogleAuthProvider();
+  export {auth , provider };
+
+  export default db;
